3d4a9e329492faa7c15b3292b1ebe7ff7a955bd4
AgustinRH <agusrubher@gmail.com>
2024-09-30 13:34:43 +0200

    Programa Omirps Finalizado

--- Tema 3/Omirps.java
+++ Tema 3/Omirps.java
@@ -3,29 +3,41 @@ import java.util.Scanner;
 public class Omirps {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        int omirp;
+        int num1, num2;
+        String numero1,inverso = "";
 
         System.out.println("Introduce un número: ");
-        omirp = sc.nextInt();
+        num1 = sc.nextInt();
         boolean primo = true;
 
-        for(int i = 0; i <= omirp; i++) {
-            for(int j = 2; j <= omirp/2;) {
-                if(omirp % j == 0) {
-                    primo = false;
-                break;
-                }
-            }
-            if (primo) {
-                // DAR la Vuelta al número
-
+        for (int i = 2; i <= num1/2; i++){
+            if(num1 % i == 0){
+                primo = false;
+            break; 
             }
         }
-        // Comprobaciones para el funcionamiento de los primos.
-        /*if (primo) {
-            System.out.println("Es primo" + omirp);
-        } else {
-            System.out.println("No es primo" + omirp);
-        }*/
+        if (primo) {
+            numero1 = Integer.toString(num1);
+            for (int k = numero1.length() - 1; k >= 0; k --) {
+                inverso = inverso + numero1.charAt(k);
+            }
+
+            num2 = Integer.parseInt(inverso);
+            
+            boolean primo2 = true;
+            for(int j = 2; j <= num2/2; j++){
+                if(num2 % j == 0) {
+                    primo2 = false;
+                }  
+            }
+
+            if (primo2){
+                System.out.println("El número " + inverso + " ES Omirp");
+            }
+            else{
+                System.out.println("El número " + inverso + "NO es Omirp");
+            }
+        }
+        sc.close();
     }
 }
