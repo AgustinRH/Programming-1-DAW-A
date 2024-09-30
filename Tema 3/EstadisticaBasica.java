f1c604c40da660268066d37e8751df4d5528850a
AgustinRH <agusrubher@gmail.com>
2024-09-30 12:45:46 +0200

    Cambios en Estadística Básica

--- Tema 3/EstadisticaBasica.java
+++ Tema 3/EstadisticaBasica.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class EstadisticaBasica {
     public static void main (String [] args) {
         Scanner sc = new Scanner(System.in);
-        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE, med = 0, num;
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE, num;
+        double med = 0;
         
         for(int i = 1; i <= 10; i++) {
             System.out.println("Inserte el número " + i + "º");
@@ -18,7 +19,7 @@ public class EstadisticaBasica {
                 min = num;
             }
         }
-        med = med / 5;
+        med = med / 10;
         System.out.println("El número máximo es " + max);
         System.out.println("El número mínimo es " + min);
         System.out.println("La media de los números es " + med);
