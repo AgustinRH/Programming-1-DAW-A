e9b7e401223867d359e5efb1cf511f87fbc8c594
AgustinRH <agusrubher@gmail.com>
2024-10-01 18:48:02 +0200

    Añadimos el programa de 'Máquina expendedora'.

---
+++ Tema 3/MaquinaExpendedora.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class MaquinaExpendedora {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double dinero, resto;
+        int euros, cincuent = 0, venti = 0, ten = 0, five = 0, two = 0, one = 0;
+        System.out.println("Ingrese la cantidad de dinero: ");
+        dinero = sc.nextDouble();
+
+        euros = (int)dinero;
+        System.out.println(euros);
+
+        resto = (dinero - (double)euros);
+        resto = resto * 100;
+        if (resto >= 50) { //Monedas de 50 céntimos hacia abajo
+            cincuent = (int)resto / 50;
+            resto -= (50*(double)cincuent);
+            if(resto >= 20) {
+                venti = (int)resto / 20;
+                resto -= (20*(double)venti);
+            } else if(resto >= 10) {
+                ten = (int)resto / 10;
+                resto -= (10*(double)ten);
+            } else if(resto >= 5) {
+                five = (int)resto / 5;
+                resto -= (5*(double)five);
+            } else if(resto >= 2) {
+                two = (int)resto / 2;
+                resto -= (2*(double)two);
+            } else if(resto >= 1) {
+                one = (int)resto/1;
+                resto -= (1*(double)one);
+            }
+        } else if(resto >= 20) { //Monedas de 20 céntimos hacia abajo
+            venti = (int)resto / 20;
+            resto -= (20*(double)venti);
+            if(resto >= 10) {
+                ten = (int)resto / 10;
+                resto -= (10*(double)ten);
+            } else if(resto >= 5) {
+                five = (int)resto / 5;
+                resto -= (5*(double)five);
+            } else if(resto >= 2) {
+                two = (int)resto / 2;
+                resto -= (2*(double)two);
+            } else if(resto >= 1) {
+                one = (int)resto/1;
+                resto -= (1*(double)one);
+            }
+        } else if(resto >= 10) { //Monedas de 10 céntimos hacia abajo
+            ten = (int)resto / 10;
+            resto -= (10*(double)ten);
+            if(resto >= 5) {
+                five = (int)resto / 5;
+                resto -= (5*(double)five);
+            } else if(resto >= 2) {
+                two = (int)resto / 2;
+                resto -= (2*(double)two);
+            } else if(resto >= 1) {
+                one = (int)resto/1;
+                resto -= (1*(double)one);
+            }
+        } else if(resto >= 5) { //Monedas de 5 céntimos hacia abajo
+            five = (int)resto / 5;
+            resto -= (5*(double)five);
+            if(resto >= 2) {
+                two = (int)resto / 2;
+                resto -= (2*(double)two);
+            } else if(resto >= 1) {
+                one = (int)resto/1;
+                resto -= (1*(double)one);
+            }
+        } else if(resto >= 2) { //Monedas de 2 céntimos hacia abajo
+            two = (int)resto / 2;
+            resto -= (2*(double)two);
+
+            if(resto >= 1) {
+                one = (int)resto/1;
+                resto -= (1*(double)one);
+            }
+        } else if(resto >= 1) { //Monedas de 1 céntimos
+            one = (int)resto/1;
+            resto -= (1*(double)one);
+        }
+
+        System.out.println("Monedas de euro: " + euros);
+        System.out.println("Monedas de 50 cents: " + cincuent);
+        System.out.println("Monedas de 20 cents: " + venti);
+        System.out.println("Monedas de 10 cents: " + ten);
+        System.out.println("Monedas de 5 cents: " + five);
+        System.out.println("Monedas de 2 cents: " + two);
+        System.out.println("Monedas de 1 cents: " + one);
+
+        sc.close();
+    }
+}
