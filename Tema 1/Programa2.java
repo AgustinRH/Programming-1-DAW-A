ec74d3b23ea6e56c873418ee74b50ed38a5eaafc
AgustinRH <agusrubher@gmail.com>
2024-10-03 12:16:50 +0200

    cambio

--- Tema 1/Programa2.java
+++ Tema 1/Programa2.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
 public class Programa2 {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        boolean control = true;
-        int num, suma = 0;
 
-        while(control) {
-            System.out.println("Inserte un número: ");
-            num = sc.nextInt();
-            if (num < 0 ) {
-                control = false;
-                System.out.println("Saliendo del programa...");
-            } else {
-            suma += num;
-            System.out.println("El total es " + suma);
+        int before = 0;
+        int now;
+
+        System.out.println("Introduce números enteros. Si deseas terminar introduce un número negativo");
+
+        do {
+            System.out.print("Introduce un número: ");
+            now = sc.nextInt();
+
+            if (now >= 0) {
+                System.out.println("La suma es: " + (before + now));
             }
-            
 
-        }
+            before = now;
+        } while (now >= 0);
+
+        System.out.println("Saliendo...");
         sc.close();
     }
 }
