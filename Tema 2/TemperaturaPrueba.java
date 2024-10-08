97f51b95a42718ae1a1d926f70950c6ad5a735df
AgustinRH <agusrubher@gmail.com>
2024-10-08 14:13:15 +0200

    Cambiossss

--- Tema 2/TemperaturaPrueba.java
+++ Tema 2/TemperaturaPrueba.java
@@ -1,16 +1,31 @@
 import java.util.Scanner;
 
-
 public class TemperaturaPrueba {
     
     public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cambio;
 
-        double cambio;
+        Temperatura cambioTemp = new Temperatura();
 
-        Temperatura convTemp = new Temperatura();
+        System.out.println("Que quieres convertir(1 = A Celsius, 2 = A Farenheit): ");
+        cambio = sc.nextInt();
 
-        cambio = convTemp.celsiusToFarenheit(25);
+        switch (cambio) {
+            case 1:
+                System.out.println("Inserta la temperatura en Farenheit");
+                double tempC = sc.nextDouble();
+                System.out.println("La temperatura en Celsius es de " + cambioTemp.farenheitToCelsius(tempC) + " ºC");
+                break;
+            case 2:
+                System.out.println("Inserta la temperatura en celsius");
+                double tempF = sc.nextDouble();
+                System.out.println("La temperatura en Farenheit es de " + cambioTemp.celsiusToFarenheit(tempF) + " ºF");
+                break;
+            default:
+                break;
+        }
 
-        System.Out.Println(cambio);
+        sc.close();
     }
 }
--- Tema 3/MaquinaExpendedora.java
+++ Tema 3/MaquinaExpendedora.java
@@ -1,66 +1,76 @@
 import java.util.Scanner;
 
 public class MaquinaExpendedora {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double dinero, resto;
-        int euros, cincuent = 0, venti = 0, ten = 0, five = 0, two = 0, one = 0, bfive = 0, bten = 0, bventi = 0;
+        int deuros = 0, euros, cincuent = 0, venti = 0, ten = 0, five = 0, two = 0, one = 0, bfive = 0, bten = 0,
+                bventi = 0;
         System.out.println("Ingrese la cantidad de dinero: ");
         dinero = sc.nextDouble();
 
-        euros = (int)dinero; // Parte entera
-        
+        euros = (int) dinero; // Parte entera
+
         resto = (dinero - euros) * 100; // Parte decimal en céntimos
         resto = Math.round(resto);
 
-        if (resto >= 50) { //Monedas de 50 céntimos 
-            cincuent = (int)resto / 50;
-            resto -= (50*(double)cincuent);
-           
-        } 
-        if(resto >= 20) { //Monedas de 20 céntimos 
-            venti = (int)resto / 20;
-            resto -= (20*(double)venti);
-           
-        }if(resto >= 10) { //Monedas de 10 céntimos 
-            ten = (int)resto / 10;
-            resto -= (10*(double)ten);
-            
-        } 
-        if(resto >= 5) { //Monedas de 5 céntimos 
-            five = (int)resto / 5;
-            resto -= (5*(double)five);
-        }  
-        
-        if(resto >= 2) { //Monedas de 2 céntimos 
-            two = (int)resto / 2;
-            resto -= (2*(double)two);
-        } 
-        
-        if(resto >= 1) { //Monedas de 1 céntimos
-            one = (int)resto/1;
-            resto -= (1*(double)one);
-        } 
+        if (resto >= 50) { // Monedas de 50 céntimos
+            cincuent = (int) resto / 50;
+            resto -= (50 * (double) cincuent);
 
-        if(euros >= 20) { // Billetes de 20 euros
+        }
+        if (resto >= 20) { // Monedas de 20 céntimos
+            venti = (int) resto / 20;
+            resto -= (20 * (double) venti);
+
+        }
+        if (resto >= 10) { // Monedas de 10 céntimos
+            ten = (int) resto / 10;
+            resto -= (10 * (double) ten);
+
+        }
+        if (resto >= 5) { // Monedas de 5 céntimos
+            five = (int) resto / 5;
+            resto -= (5 * (double) five);
+        }
+
+        if (resto >= 2) { // Monedas de 2 céntimos
+            two = (int) resto / 2;
+            resto -= (2 * (double) two);
+        }
+
+        if (resto >= 1) { // Monedas de 1 céntimos
+            one = (int) resto / 1;
+            resto -= (1 * (double) one);
+        }
+
+        
+
+        if (euros >= 20) { // Billetes de 20 euros
             bventi = euros / 20;
             euros -= (20 * bventi);
         }
 
-        if(euros >= 10) { // Billetes de 10 euros
+        if (euros >= 10) { // Billetes de 10 euros
             bten = euros / 10;
             euros -= (10 * bten);
         }
 
-        if(euros >= 5) { // Billetes de 5 euros
+        if (euros >= 5) { // Billetes de 5 euros
             bfive = euros / 5;
             euros -= (5 * bfive);
         }
 
+        if (euros >= 2) {
+            deuros = euros / 2;
+            euros -= (2 * deuros);
+        }
         System.out.println("--------------------------");
         System.out.println("Billetes de 20 euros: " + bventi);
         System.out.println("Billetes de 10 euros: " + bten);
         System.out.println("Billetes de 5 euros: " + bfive);
+
+        System.out.println("Monedas de 2 euros: " + deuros);
         System.out.println("Monedas de 1 euro: " + euros);
         System.out.println("Monedas de 50 cents: " + cincuent);
         System.out.println("Monedas de 20 cents: " + venti);
@@ -68,7 +78,6 @@ public class MaquinaExpendedora {
         System.out.println("Monedas de 5 cents: " + five);
         System.out.println("Monedas de 2 cents: " + two);
         System.out.println("Monedas de 1 cents: " + one);
-
 
         sc.close();
     }
