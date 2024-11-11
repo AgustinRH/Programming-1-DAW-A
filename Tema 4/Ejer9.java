32ba2301750b99c9b2c0f1d3dd9755f07edde934
AgustinRH <agusrubher@gmail.com>
2024-11-11 13:08:15 +0100

    Algo

---
+++ Tema 4/Ejer9.java
@@ -0,0 +1,9 @@
+public class Ejer9 {
+    private int[] array;
+
+    public void generarArray(int tam, int min, int max) {
+        for (int i = 0; i < array.length; i++) {
+            
+        }
+    }
+}
