cace72710f7baf24fb28e587820c03960c02cb74
AgustinRH <agusrubher@gmail.com>
2024-11-19 13:48:15 +0100

    Carta

--- Tema 4/PracticaObligatoria/Carta.java
+++ Tema 4/PracticaObligatoria/Carta.java
@@ -1,30 +1,61 @@
 package PracticaObligatoria;
 
 public class Carta {
-    private int valor = -1;
-    private String palo = "nulo";
 
-    public Carta(int valor, String palo){
+    // Variables privadas de la clase.
+    private String valor, palo;
+    private int tipo;
+
+    // Constructor de la clase para dar valor a las variables.
+    public Carta(String valor, String palo, int tipo) {
         this.valor = valor;
         this.palo = palo;
+        this.tipo = tipo;
     }
-    public int getValor() {
+
+    // Getter de Tipo
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getValor() {
         return valor;
     }
 
-    
-    public void setValor(int valor) {
-        if (valor > 0 && valor < 13) {
-            this.valor = valor;
-        } 
+    public void setValor(String valor, int tipo) {
+        String valoresValidos;
+        if (tipo == 1) {
+            valoresValidos = "1234567SCR";
+            if (valoresValidos.contains(valor)) {
+                this.valor = valor;
+            }
+        }
+        if (tipo == 2) {
+            valoresValidos = "A23456789JQK";
+            if (valoresValidos.contains(valor) || valor.equals("10")) {
+                this.valor = valor;
+            }
+        }
     }
 
     public String getPalo() {
         return palo;
     }
 
-    public void setPalo (String palo) {
+    public void setPalo(String palo) {
+        // Baraja de póker
+        if (palo.equals("CORAZONES") || palo.equals("DIAMANTES") || palo.equals("PICAS") || palo.equals("TREBOLES")) {
             this.palo = palo;
+        }
+
+        //Baraja española
+        if (palo.equals("BASTOS") || palo.equals("OROS")||palo.equals("ESPADAS") ||palo.equals("COPAS")) {
+            this.palo = palo;
+        }
     }
 
 }
