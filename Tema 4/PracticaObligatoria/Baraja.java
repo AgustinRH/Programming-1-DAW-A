59a2d6204d7bf893acb0b66a17c4c4af740c3df8
AgustinRH <agusrubher@gmail.com>
2024-11-19 18:29:32 +0100

    Cambios en la práctica de la unidad 4

--- Tema 4/PracticaObligatoria/Baraja.java
+++ Tema 4/PracticaObligatoria/Baraja.java
@@ -1,6 +1,6 @@
 package PracticaObligatoria;
 
 public class Baraja {
-    
+    private Carta c = new Carta(null, null, 0);
 
 }
--- Tema 4/PracticaObligatoria/Carta.java
+++ Tema 4/PracticaObligatoria/Carta.java
@@ -18,22 +18,29 @@ public class Carta {
         return tipo;
     }
 
+    // Setter de Tipo
     public void setTipo(int tipo) {
         this.tipo = tipo;
     }
 
+    // Getter de Valor
     public String getValor() {
         return valor;
     }
 
+    // Setter de Valor
     public void setValor(String valor, int tipo) {
         String valoresValidos;
+
+        // Si el tipo es Baraja Española
         if (tipo == 1) {
             valoresValidos = "1234567SCR";
             if (valoresValidos.contains(valor)) {
                 this.valor = valor;
             }
         }
+
+        // Si el tipo es Baraja de Poker
         if (tipo == 2) {
             valoresValidos = "A23456789JQK";
             if (valoresValidos.contains(valor) || valor.equals("10")) {
@@ -42,10 +49,12 @@ public class Carta {
         }
     }
 
+    // Getter de Palo
     public String getPalo() {
         return palo;
     }
 
+    // Setter de Palo
     public void setPalo(String palo) {
         // Baraja de póker
         if (palo.equals("CORAZONES") || palo.equals("DIAMANTES") || palo.equals("PICAS") || palo.equals("TREBOLES")) {
@@ -58,10 +67,12 @@ public class Carta {
         }
     }
 
+    // Devuelve un String del valor y el palo de la carta.
     public String toString() {
         return "Valor" + valor + " | " + "Palo: " + palo;
     }
 
+    //Método para comparar cartas
     public boolean equals(Carta c) {
         boolean iguales = false;
         if (c.getValor().equals(this.valor) || c.getPalo().equals(this.palo)) {
@@ -70,12 +81,22 @@ public class Carta {
         return iguales;
     }
 
+    // Método para clonar cartas
     public Carta clone(){
         Carta c = new Carta(this.valor, this.palo, this.tipo);
         return c;
     }
 
-    public void compararPalos(Carta c){
-        
+    // Método para comparar palos de cartas
+    public boolean compararPalos(Carta c){
+        return this.palo.equals(c.palo);
+    }
+
+    // Método para saber si una carta es mayor que otra
+    public boolean mayorQue(Carta c) {
+        boolean mayor = false;
+        if (this.valor.c.valor) {
+            mayor = true;
+        }
     }
 }
