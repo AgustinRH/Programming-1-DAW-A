f38673ca4f7b293545ad3dd5c47a4f64fc546477
AgustinRH <agusrubher@gmail.com>
2024-11-22 12:35:36 +0100

    Algo

--- Tema 4/PracticaObligatoria/Carta.java
+++ Tema 4/PracticaObligatoria/Carta.java
@@ -72,31 +72,37 @@ public class Carta {
         return "Valor" + valor + " | " + "Palo: " + palo;
     }
 
-    //Método para comparar cartas
+    // Método para comparar cartas
     public boolean equals(Carta c) {
         boolean iguales = false;
-        if (c.getValor().equals(this.valor) || c.getPalo().equals(this.palo)) {
+        if (c.getValor().equals(this.valor) && c.getPalo().equals(this.palo)) {
             iguales = true;
         }
         return iguales;
     }
 
     // Método para clonar cartas
-    public Carta clone(){
+    public Carta clone() {
         Carta c = new Carta(this.valor, this.palo, this.tipo);
         return c;
     }
 
+    // Método para comparar números de cartas
+    public boolean compararNumeros(Carta c) {
+        return this.valor.equals(c.valor);
+    }
+
     // Método para comparar palos de cartas
-    public boolean compararPalos(Carta c){
+    public boolean compararPalos(Carta c) {
         return this.palo.equals(c.palo);
     }
 
     // Método para saber si una carta es mayor que otra
     public boolean mayorQue(Carta c) {
         boolean mayor = false;
-        if (this.valor.c.valor) {
+        if (this.valor.compareTo(c.getValor()) > 0) {
             mayor = true;
         }
+        return mayor;
     }
 }
